c8e2af957628b093a5ef618f32ced1c5b0cee0b7
VeselovTimofey <vestimofey@mail.ru>
2024-06-17 01:59:30 +0300

    update: eslint airbnb

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,17 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'airbnb-base',
+    'airbnb-typescript',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  plugins: ['react-refresh', '@typescript-eslint'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
