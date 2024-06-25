861f25c745ece22a1bd6cc4ca3de2001c1aa8f14
Semyon Katz <kavabunga@gmail.com>
2024-06-25 16:31:13 +0300

    refactor: :recycle: refactor routing, add notes for next tasks

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,15 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/catalogue/:category?">
+          {/* // TODO: Add data prefetching in Route component  */}
+          {/* // TODO: Use param to filter request (on router side) */}
           <Route index Component={CataloguePage} />
         </Route>
         {/* NOTE: Product page */}
         <Route path="/products/:productId" element={<div>Artwork page</div>} />
         {/* TODO: Test page. Remove after testing */}
         <Route path="/test" element={<div>Test page</div>} />
-        <Route path="/" element={<div>Root page</div>} />
+        <Route index element={<div>Root page</div>} />
       </Routes>
     </BrowserRouter>
   );
