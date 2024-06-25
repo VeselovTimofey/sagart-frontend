d7de86b55e40be15df9d42f686a1ad07b00af636
Semyon Katz <kavabunga@gmail.com>
2024-06-25 16:29:37 +0300

    feat: :sparkles: impliment filters bar widget on catalogue page

--- src/pages/Catalogue/index.tsx
+++ src/pages/Catalogue/index.tsx
@@ -1,7 +1,3 @@
-// TODO: Here to add Catalogue page.
-// TODO: Add data prefetching in Route component
-// TODO: Use param to filter request (on router side)
-
 import { useParams } from 'react-router-dom';
 
 import { Box, Stack, Typography } from '@mui/material';
@@ -10,6 +6,7 @@ import ProductsList from '../../components/ProductsList';
 import artworks from '../../utils/mock/artworks';
 import BreadcrumbsWidget from '../../components/BreadcrumbsWidget';
 import breadcrumbNameMap from '../../utils/const';
+import FilterBarWidget from '../../components/FilterBarWidget';
 
 export default function CataloguePage() {
   const { category } = useParams();
@@ -18,29 +15,28 @@ export default function CataloguePage() {
     : 'Каталог';
 
   return (
-    <Stack direction="column">
-      <Stack direction="column">
+    <Stack direction="column" px="3.75rem" gap={7.5}>
+      <Stack direction="column" gap={3}>
         <BreadcrumbsWidget />
-        <Typography variant="h1">{name}</Typography>
+        <Typography variant="h1" fontSize={32}>
+          {name}
+        </Typography>
       </Stack>
-      <Stack direction="row">
+      <Stack direction="row" gap={5}>
         <Box>
           <Box
             position="sticky"
             top={20}
             sx={{
-              width: 312,
-              backgroundColor: 'indigo',
-              minHeight: 800,
+              width: 296,
+              overflowY: 'scroll',
             }}
           >
-            <Typography variant="h5" color="white">
-              Фильтры
-            </Typography>
+            <FilterBarWidget />
           </Box>
         </Box>
-        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          <ProductsList products={artworks} amountToShow={20} size="large" />
+        <Box component="main" sx={{ flexGrow: 1 }}>
+          <ProductsList products={artworks} amountToShow={20} size="medium" />
         </Box>
       </Stack>
     </Stack>
