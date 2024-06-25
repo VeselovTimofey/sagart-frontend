5cc9f0dd44a7ff29f971befd7f4ac89bc85a5f4a
Semyon Katz <kavabunga@gmail.com>
2024-06-25 13:55:15 +0300

    feat: :sparkles: add basic layout for catalogue page

--- src/pages/Catalogue/index.tsx
+++ src/pages/Catalogue/index.tsx
@@ -1,15 +1,48 @@
 // TODO: Here to add Catalogue page.
-// TODO: Add param with category
 // TODO: Add data prefetching in Route component
+// TODO: Use param to filter request (on router side)
 
-import { Stack } from '@mui/material';
+import { useParams } from 'react-router-dom';
+
+import { Box, Stack, Typography } from '@mui/material';
+
 import ProductsList from '../../components/ProductsList';
-import { artworks } from '../../utils/mock/artworks';
+import artworks from '../../utils/mock/artworks';
+import BreadcrumbsWidget from '../../components/BreadcrumbsWidget';
+import breadcrumbNameMap from '../../utils/const';
 
 export default function CataloguePage() {
+  const { category } = useParams();
+  const name = category
+    ? breadcrumbNameMap[`/catalogue/${category}`]
+    : 'Каталог';
+
   return (
-    <Stack>
-      <ProductsList products={artworks} amountToShow={8} size="large" />
+    <Stack direction="column">
+      <Stack direction="column">
+        <BreadcrumbsWidget />
+        <Typography variant="h1">{name}</Typography>
+      </Stack>
+      <Stack direction="row">
+        <Box>
+          <Box
+            position="sticky"
+            top={20}
+            sx={{
+              width: 312,
+              backgroundColor: 'indigo',
+              minHeight: 800,
+            }}
+          >
+            <Typography variant="h5" color="white">
+              Фильтры
+            </Typography>
+          </Box>
+        </Box>
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <ProductsList products={artworks} amountToShow={20} size="large" />
+        </Box>
+      </Stack>
     </Stack>
   );
 }
