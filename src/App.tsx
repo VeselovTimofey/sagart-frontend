78f5d6621bc7b73ae12fcfd0cb9eb56e80a446e3
VeselovTimofey <vestimofey@mail.ru>
2024-06-27 04:12:20 +0300

    feat: create Header

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CataloguePage from './pages/Catalogue';
 import ProductPage from './pages/Product';
 
+import Header from './components/Header/index';
 import Footer from './components/Footer/index';
 
 function App() {
   return (
     <BrowserRouter>
+      {/* // TODO: Create layout with Header and Footer */}
+      <Header />
       <Routes>
         <Route path="/catalogue/:category?">
           {/* // TODO: Add data prefetching in Route component  */}
---
+++ src/components/Header/ui/index.tsx
@@ -0,0 +1,48 @@
+import {
+  AppBar,
+  Button,
+  Grid,
+  InputBase,
+  Link,
+  Typography,
+  Toolbar,
+} from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+
+import logoPath from '../../../images/svg/logo.svg';
+import HEADERLINK from '../../../utils/constant/headerLink';
+
+function HeaderUi() {
+  return (
+    <AppBar color="transparent" position="static">
+      <Grid direction="column" container>
+        <Grid item>
+          <Toolbar>
+            <Link href="#">
+              <img src={logoPath} alt="Логотип сайта." />
+            </Link>
+            <SearchIcon />
+            <InputBase placeholder="Поиск" />
+            <Button sx={{ ml: 'auto' }}>Регистрация</Button>
+            <FavoriteBorderIcon />
+            <ShoppingBasketIcon />
+          </Toolbar>
+        </Grid>
+        <Grid item justifyContent="space-evenly" container>
+          {/* //TODO: Moving this map to a separate file */}
+          {HEADERLINK.map((link) => (
+            <Grid item key={link.name}>
+              <Link href={link.link}>
+                <Typography component="p">{link.name}</Typography>
+              </Link>
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+    </AppBar>
+  );
+}
+
+export default HeaderUi;
