f574584b0c7a6b80bd0747a0b57e3807ba88c983
VeselovTimofey <vestimofey@mail.ru>
2024-06-30 19:43:57 +0300

    feat: add ShowCase

--- src/pages/Root/index.tsx
+++ src/pages/Root/index.tsx
@@ -2,15 +2,26 @@ import { Grid } from '@mui/material';
 
 import LastNews from '../../components/LastNews/index';
 import Categories from '../../components/Сategories/index';
+import ShowCase from '../../components/Showcase';
 
 function MainPage() {
   return (
-    <Grid component="main" alignItems="center" container>
+    /* //TODO: Set the width of main and its section to certain sizes */
+    <Grid component="main" direction="column" alignItems="center" container>
       <Grid item>
         <LastNews />
       </Grid>
       <Grid item>
         <Categories />
+      </Grid>
+      <Grid style={{ width: '1321px' }} item>
+        <ShowCase title="Новинки" link="#" />
+      </Grid>
+      <Grid style={{ width: '1321px' }} item>
+        <ShowCase title="Работы до 50 000 рублей" link="#" />
+      </Grid>
+      <Grid style={{ width: '1321px' }} item>
+        <ShowCase title="Работы до 500 000 рублей" link="#" />
       </Grid>
     </Grid>
   );
