79ca318bf3dcba05f6f4f56345bc245bdf46e5ef
Semyon Katz <kavabunga@gmail.com>
2024-07-03 00:10:40 +0300

    feat: :sparkles: add mui theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import { CssBaseline, ThemeProvider } from '@mui/material';
+
+import 'typeface-inter';
+
+import { theme } from './utils/styles/theme';
+
 import Header from './components/Header/index';
 import Footer from './components/Footer/index';
 
@@ -11,21 +17,24 @@ import AuthorPage from './pages/Author';
 function App() {
   return (
     <BrowserRouter>
-      {/* // TODO: Create layout with Header and Footer */}
-      <Header />
-      <Routes>
-        {/* // TODO: Add data prefetching in Route component  */}
-        {/* // TODO: Use param to filter request (on router side) */}
-        <Route path="/catalogue/:category?" Component={CataloguePage} />
-        {/* NOTE: Product page */}
-        <Route path="/products/:productId" Component={ProductPage} />
-        {/* NOTE: Author page */}
-        <Route path="/authors/:authorId" Component={AuthorPage} />
-        {/* TODO: Test page. Remove after testing */}
-        <Route path="/test" element={<div>Test page</div>} />
-        <Route index Component={MainPage} />
-      </Routes>
-      <Footer />
+      <ThemeProvider {...{ theme }}>
+        <CssBaseline />
+        {/* // TODO: Create layout with Header and Footer */}
+        <Header />
+        <Routes>
+          {/* // TODO: Add data prefetching in Route component  */}
+          {/* // TODO: Use param to filter request (on router side) */}
+          <Route path="/catalogue/:category?" Component={CataloguePage} />
+          {/* NOTE: Product page */}
+          <Route path="/products/:productId" Component={ProductPage} />
+          {/* NOTE: Author page */}
+          <Route path="/authors/:authorId" Component={AuthorPage} />
+          {/* TODO: Test page. Remove after testing */}
+          <Route path="/test" element={<div>Test page</div>} />
+          <Route index Component={MainPage} />
+        </Routes>
+        <Footer />
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
